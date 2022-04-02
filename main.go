fafa8a9daead9b81cb2255bb8da1bdc4176c015e
Jeremy Saenz <jeremy.saenz@gmail.com>
2022-04-02 15:10:24 -0700

    Update main func

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "github.com/codegangsta/ck3_language_server/parser"
+import (
+	"fmt"
+
+	"github.com/codegangsta/ck3_language_server/parser"
+)
 
 func main() {
-	parser.ParseFile("parser/example.go")
+	ast, _ := parser.ParseFile("parser/test_cases/example.txt")
+	fmt.Printf("%#v", ast)
 }
